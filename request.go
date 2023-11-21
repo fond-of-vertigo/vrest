ab3627bcbfb685911372c0bda354bff1f1776940
Andre Dankbar <andre.dankbar@fondof.de>
2023-11-21 17:19:12 +0100

    feat: add support for ReadCloser

--- request.go
+++ request.go
@@ -168,8 +168,6 @@ func (req *Request) SetResponseBody(value interface{}) *Request {
 	req.Response.Body = value
 	if req.Response.WantsReadCloser() {
 		req.Response.CloseBody = false
-		req.Response.TraceBody = false
-		req.Response.DoUnmarshal = false
 	}
 	return req
 }
