626c427ed0861fce357b7e9e14f07faf868f85ab
Andre Dankbar <andre.dankbar@fondof.de>
2024-08-26 12:01:21 +0200

    refactor!: add format string functions for req.Do

--- do.go
+++ do.go
@@ -31,45 +31,86 @@ func Do(req *Request) error {
 	return req.Response.Error
 }
 
-func (req *Request) DoGet(pathFormat string, values ...any) error {
-	return req.Do(http.MethodGet, pathFormat, values...)
+func (req *Request) DoGet(path string) error {
+	return req.Do(http.MethodGet, path)
 }
 
-func (req *Request) DoHead(pathFormat string, values ...any) error {
-	return req.Do(http.MethodHead, pathFormat, values...)
+func (req *Request) DoGetf(pathFormat string, values ...any) error {
+	return req.Dof(http.MethodGet, pathFormat, values...)
 }
 
-func (req *Request) DoPost(pathFormat string, values ...any) error {
-	return req.Do(http.MethodPost, pathFormat, values...)
+func (req *Request) DoHead(path string) error {
+	return req.Do(http.MethodHead, path)
 }
 
-func (req *Request) DoPut(pathFormat string, values ...any) error {
-	return req.Do(http.MethodPut, pathFormat, values...)
+func (req *Request) DoHeadf(pathFormat string, values ...any) error {
+	return req.Dof(http.MethodHead, pathFormat, values...)
 }
 
-func (req *Request) DoPatch(pathFormat string, values ...any) error {
-	return req.Do(http.MethodPatch, pathFormat, values...)
+func (req *Request) DoPost(path string) error {
+	return req.Do(http.MethodPost, path)
 }
 
-func (req *Request) DoDelete(pathFormat string, values ...any) error {
-	return req.Do(http.MethodDelete, pathFormat, values...)
+func (req *Request) DoPostf(pathFormat string, values ...any) error {
+	return req.Dof(http.MethodPost, pathFormat, values...)
 }
 
-func (req *Request) DoConnect(pathFormat string, values ...any) error {
-	return req.Do(http.MethodConnect, pathFormat, values...)
+func (req *Request) DoPut(path string) error {
+	return req.Do(http.MethodPut, path)
 }
 
-func (req *Request) DoOptions(pathFormat string, values ...any) error {
-	return req.Do(http.MethodOptions, pathFormat, values...)
+func (req *Request) DoPutf(pathFormat string, values ...any) error {
+	return req.Dof(http.MethodPut, pathFormat, values...)
 }
 
-func (req *Request) DoTrace(pathFormat string, values ...any) error {
-	return req.Do(http.MethodTrace, pathFormat, values...)
+func (req *Request) DoPatch(path string) error {
+	return req.Do(http.MethodPatch, path)
 }
 
-func (req *Request) Do(method, pathFormat string, values ...any) error {
+func (req *Request) DoPatchf(pathFormat string, values ...any) error {
+	return req.Dof(http.MethodPatch, pathFormat, values...)
+}
+
+func (req *Request) DoDelete(path string) error {
+	return req.Do(http.MethodDelete, path)
+}
+
+func (req *Request) DoDeletef(pathFormat string, values ...any) error {
+	return req.Dof(http.MethodDelete, pathFormat, values...)
+}
+
+func (req *Request) DoConnect(path string) error {
+	return req.Do(http.MethodConnect, path)
+}
+
+func (req *Request) DoConnectf(pathFormat string, values ...any) error {
+	return req.Dof(http.MethodConnect, pathFormat, values...)
+}
+
+func (req *Request) DoOptions(path string) error {
+	return req.Do(http.MethodOptions, path)
+}
+
+func (req *Request) DoOptionsf(pathFormat string, values ...any) error {
+	return req.Dof(http.MethodOptions, pathFormat, values...)
+}
+
+func (req *Request) DoTrace(path string) error {
+	return req.Do(http.MethodTrace, path)
+}
+
+func (req *Request) DoTracef(pathFormat string, values ...any) error {
+	return req.Dof(http.MethodTrace, pathFormat, values...)
+}
+
+func (req *Request) Dof(method, pathFormat string, values ...any) error {
+	path := fmt.Sprintf(pathFormat, values...)
+	return req.Do(method, path)
+}
+
+func (req *Request) Do(method, path string) error {
 	req.Method = method
-	req.Path = fmt.Sprintf(pathFormat, values...)
+	req.Path = path
 	return req.Client.Overridable.Do(req)
 }
 
